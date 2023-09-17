d72cea5fca1bdb15b9591e60fabd7949c9ffe15e
Faisal <fasinadem108@gmail.com>
2023-09-17 15:02:36 +0500

    fffff

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "animate.css";
 import "animate.css/animate.min.css";
-import ScrollAnimation from "react-animate-on-scroll";
 const Services = () => {
   return (
     <div className="">
@@ -22,141 +21,138 @@ const Services = () => {
           </div>
         </div>
       </section>
+      {/*  */}{" "}
+      <div className="md:flex px-5 md:px-0 gap-5 m-auto max-w-[70rem]  animate__animated animate__fadeInLeft">
+        <a
+          className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
+          href="#"
+        >
+          <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
+            <img
+              className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
+              src="../docs/assets/img/500x300/img1.jpg"
+              alt="Image Description"
+            />
+          </div>
+          <div className="p-4 md:p-5">
+            <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+              Basic Islamic Concepts
+            </h3>
+            <p className="mt-1 text-gray-800 dark:text-gray-400">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </a>
+        <a
+          className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
+          href="#"
+        >
+          <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
+            <img
+              className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
+              src="../docs/assets/img/500x300/img1.jpg"
+              alt="Image Description"
+            />
+          </div>
+          <div className="p-4 md:p-5">
+            <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+              Basic Norani & Madani Qaida
+            </h3>
+            <p className="mt-1 text-gray-800 dark:text-gray-400">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </a>
+        <a
+          className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
+          href="#"
+        >
+          <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
+            <img
+              className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
+              src="../docs/assets/img/500x300/img1.jpg"
+              alt="Image Description"
+            />
+          </div>
+          <div className="p-4 md:p-5">
+            <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+              Recitation Quran with Tajweed
+            </h3>
+            <p className="mt-1 text-gray-800 dark:text-gray-400">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </a>
+      </div>
       {/*  */}
-      <ScrollAnimation animateIn="animate__animated animate__lightSpeedInRight">
-        {" "}
-        <div className="md:flex px-5 md:px-0 gap-5 m-auto max-w-[70rem]  animate__animated animate__fadeInLeft">
-          <a
-            className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
-            href="#"
-          >
-            <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
-              <img
-                className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
-                src="../docs/assets/img/500x300/img1.jpg"
-                alt="Image Description"
-              />
-            </div>
-            <div className="p-4 md:p-5">
-              <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-                Basic Islamic Concepts
-              </h3>
-              <p className="mt-1 text-gray-800 dark:text-gray-400">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </a>
-          <a
-            className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
-            href="#"
-          >
-            <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
-              <img
-                className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
-                src="../docs/assets/img/500x300/img1.jpg"
-                alt="Image Description"
-              />
-            </div>
-            <div className="p-4 md:p-5">
-              <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-                Basic Norani & Madani Qaida
-              </h3>
-              <p className="mt-1 text-gray-800 dark:text-gray-400">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </a>
-          <a
-            className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
-            href="#"
-          >
-            <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
-              <img
-                className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
-                src="../docs/assets/img/500x300/img1.jpg"
-                alt="Image Description"
-              />
-            </div>
-            <div className="p-4 md:p-5">
-              <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-                Recitation Quran with Tajweed
-              </h3>
-              <p className="mt-1 text-gray-800 dark:text-gray-400">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </a>
-        </div>
-        {/*  */}
-        <div className="mt-5 md:flex px-5 md:px-0 gap-5 m-auto max-w-[70rem]  animate__animated animate__fadeInLeft">
-          <a
-            className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
-            href="#"
-          >
-            <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
-              <img
-                className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
-                src="../docs/assets/img/500x300/img1.jpg"
-                alt="Image Description"
-              />
-            </div>
-            <div className="p-4 md:p-5">
-              <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-                Memorization of Quran
-              </h3>
-              <p className="mt-1 text-gray-800 dark:text-gray-400">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </a>
-          <a
-            className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
-            href="#"
-          >
-            <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
-              <img
-                className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
-                src="../docs/assets/img/500x300/img1.jpg"
-                alt="Image Description"
-              />
-            </div>
-            <div className="p-4 md:p-5">
-              <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-                Quran Translation Course
-              </h3>
-              <p className="mt-1 text-gray-800 dark:text-gray-400">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </a>
-          <a
-            className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
-            href="#"
-          >
-            <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
-              <img
-                className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
-                src="../docs/assets/img/500x300/img1.jpg"
-                alt="Image Description"
-              />
-            </div>
-            <div className="p-4 md:p-5">
-              <h3 className="text-lg font-bold text-gray-800 dark:text-white">
-                Quran & Azan with different excents
-              </h3>
-              <p className="mt-1 text-gray-800 dark:text-gray-400">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </p>
-            </div>
-          </a>
-        </div>
-      </ScrollAnimation>
+      <div className="mt-5 md:flex px-5 md:px-0 gap-5 m-auto max-w-[70rem]  animate__animated animate__fadeInLeft">
+        <a
+          className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
+          href="#"
+        >
+          <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
+            <img
+              className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
+              src="../docs/assets/img/500x300/img1.jpg"
+              alt="Image Description"
+            />
+          </div>
+          <div className="p-4 md:p-5">
+            <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+              Memorization of Quran
+            </h3>
+            <p className="mt-1 text-gray-800 dark:text-gray-400">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </a>
+        <a
+          className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
+          href="#"
+        >
+          <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
+            <img
+              className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
+              src="../docs/assets/img/500x300/img1.jpg"
+              alt="Image Description"
+            />
+          </div>
+          <div className="p-4 md:p-5">
+            <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+              Quran Translation Course
+            </h3>
+            <p className="mt-1 text-gray-800 dark:text-gray-400">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </a>
+        <a
+          className="flex flex-col group bg-white border shadow-sm rounded-xl overflow-hidden hover:shadow-lg transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
+          href="#"
+        >
+          <div className="relative pt-[50%] sm:pt-[60%] lg:pt-[80%] rounded-t-xl overflow-hidden">
+            <img
+              className="w-full h-full absolute top-0 left-0 object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-t-xl"
+              src="../docs/assets/img/500x300/img1.jpg"
+              alt="Image Description"
+            />
+          </div>
+          <div className="p-4 md:p-5">
+            <h3 className="text-lg font-bold text-gray-800 dark:text-white">
+              Quran & Azan with different excents
+            </h3>
+            <p className="mt-1 text-gray-800 dark:text-gray-400">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </a>
+      </div>
     </div>
   );
 };
